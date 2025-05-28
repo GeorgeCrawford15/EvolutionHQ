81b72bc790d0e13e2cd0ac67d2c84406a8408ba3
George Crawford <georgecrawford@Georges-MacBook-Pro.local>
2025-05-28 18:50:23 -0400

    create addColumn() functionality and add status box

--- static/script.js
+++ static/script.js
@@ -1,2 +1,31 @@
 // Add functionality to the buttons
 // Make it so that there's a limit of species columns that can be added
+function addRow() {
+
+}
+
+function removeRow() {
+
+}
+
+let speciesNum = 5;
+function addColumn() {
+    speciesNum++;
+    if (speciesNum < 11) {
+        const topRow = document.getElementById('top-row');
+        const newNameBox = document.createElement('th');
+        const newInput = document.createElement('input');
+        newInput.type = 'text';
+        newInput.className = 'species-name';
+        newInput.placeholder = 'Species ' + speciesNum.toString();
+        newNameBox.appendChild(newInput);
+        topRow.appendChild(newNameBox);
+    } else {
+        document.querySelector('.status-message').style.visibility = 'visible';
+        document.querySelector('.status-message').innerText = "No more species can be added. Maximum number of species is 10."
+    }
+}
+
+function removeColumn() {
+
+}
