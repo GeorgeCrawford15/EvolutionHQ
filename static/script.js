e67b06ed47fbc9ad12e448535c7493a07bd9faeb
George Crawford <georgecrawford@Mac.fios-router.home>
2025-05-29 01:34:37 -0400

    add full functionality to all four buttons that add and remove columns or rows.

--- static/script.js
+++ static/script.js
@@ -1,31 +1,95 @@
-// Add functionality to the buttons
-// Make it so that there's a limit of species columns that can be added
+let rowCount = 1;
 function addRow() {
+    rowCount++;
+    document.querySelector('.status-message').style.visibility = 'hidden';
+    const tableBody = document.querySelector('tbody');
+    const newRow = document.createElement('tr');
+    newRow.className = 'row' + rowCount;
+    const newCharBox = document.createElement('td');
+    const newCharInput = document.createElement('input');
+    newCharInput.type = 'text';
+    newCharInput.className = 'character';
+    const newStateBox = document.createElement('td');
+    const newStateInput = document.createElement('input');
+    newStateInput.type = 'text';
+    newStateInput.className = 'state';
+    newCharBox.appendChild(newCharInput);
+    newStateBox.appendChild(newStateInput);
+    newRow.appendChild(newCharBox);
+    newRow.appendChild(newStateBox);
+    tableBody.appendChild(newRow);
 
+    for (let i = 0; i < speciesCount; i++) {
+        const newBinaryBox = document.createElement('td');
+        const newBinary = document.createElement('input');
+        newBinary.type = 'number';
+        newBinary.className = 'binary';
+        newBinary.min = '0';
+        newBinary.max = '1';
+        newBinaryBox.appendChild(newBinary);
+        newRow.appendChild(newBinaryBox);
+    }
 }
 
 function removeRow() {
-
+    document.querySelector('.status-message').style.visibility = 'hidden';
+    if (rowCount > 1) {
+        bottomRow = document.querySelector('.row' + rowCount);
+        bottomRow.remove();
+        rowCount--;
+    } else {
+        document.querySelector('.status-message').style.visibility = 'visible';
+        document.querySelector('.status-message').innerText = "Already at minimum number of rows.";
+        document.querySelector('.status-message').style.color = 'red';
+    }
 }
 
-let speciesNum = 5;
+let speciesCount = 5;
 function addColumn() {
-    speciesNum++;
-    if (speciesNum < 11) {
+    document.querySelector('.status-message').style.visibility = 'hidden';
+    if (speciesCount < 10) {
+        speciesCount++;
         const topRow = document.getElementById('top-row');
         const newNameBox = document.createElement('th');
+        newNameBox.className = 'col' + speciesCount;
         const newInput = document.createElement('input');
         newInput.type = 'text';
         newInput.className = 'species-name';
-        newInput.placeholder = 'Species ' + speciesNum.toString();
+        newInput.placeholder = 'Species ' + speciesCount;
         newNameBox.appendChild(newInput);
         topRow.appendChild(newNameBox);
+
+        for (let i = 1; i <= rowCount; i++) {
+            const row = document.querySelector('.row' + i);
+            const newBinaryBox = document.createElement('td');
+            const newBinary = document.createElement('input');
+            newBinary.type = 'number';
+            newBinary.className = 'binary';
+            newBinary.min = '0';
+            newBinary.max = '1';
+            newBinaryBox.appendChild(newBinary);
+            row.appendChild(newBinaryBox);
+        }
     } else {
         document.querySelector('.status-message').style.visibility = 'visible';
-        document.querySelector('.status-message').innerText = "No more species can be added. Maximum number of species is 10."
+        document.querySelector('.status-message').innerText = "No more species can be added. Maximum number of species is 10.";
+        document.querySelector('.status-message').style.color = 'red';
     }
 }
 
 function removeColumn() {
-
+    document.querySelector('.status-message').style.visibility = 'hidden';
+    if (speciesCount > 2) {
+        const lastColumnHead = document.querySelector('.col' + speciesCount);
+        lastColumnHead.remove();
+        for (let i = 1; i <= rowCount; i++) {
+            const lastBinary = document.querySelector('.row' + i + ' td:last-of-type');
+            lastBinary.remove();
+        }
+        speciesCount--;
+    } else {
+        document.querySelector('.status-message').style.visibility = 'visible';
+        document.querySelector('.status-message').innerText = "Already at minimum number of columns. You must have two to compare.";
+        document.querySelector('.status-message').style.color = 'red';
+    }
 }
